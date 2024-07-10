eed774d51ae5b1089b159b4b838cdcaac3302ef4
Andres Reyes <areyesdev@gmail.com>
2024-07-10 16:08:22 -0500

    add: home page

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -1,68 +1,71 @@
 "use client"
 
 import { zodResolver } from "@hookform/resolvers/zod"
+import { useState } from "react";
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { Button } from "@/components/ui/button"
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import { Form } from "@/components/ui/form"
+import "react-phone-number-input/style.css";
+import CustomFormField, { FormFieldType } from "../CustomFormField"
+import SubmitButton from "../SubmitButton";
+import { UserFormValidation } from "@/lib/validation";
 
 const PatientForm = () => {
-  // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  })
+    const [isLoading, setIsLoading] = useState(false);
+    const form = useForm<z.infer<typeof UserFormValidation>>({
+        resolver: zodResolver(UserFormValidation),
+        defaultValues: {
+          name: "",
+          email: "",
+          phone: "",
+        },
+      });
 
-  // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-  }
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
-        <section className="mb-12 space-y-4">
-            <h1 className="header">Hola👋</h1>
-            <p className="text-dark-700">Agenda tu primera cita</p>
-        </section>
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
-  )
+
+    const onSubmit = async (values: z.infer<typeof UserFormValidation>) => {
+        console.log(setIsLoading)
+    }
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+                <section className="mb-12 space-y-4">
+                    <h1 className="header">Hola👋</h1>
+                    <p className="text-dark-700">Agenda tu primera cita</p>
+                </section>
+
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name='name'
+                    label='Nombre Completo'
+                    placeholder='Andres Reyes'
+                    iconSrc='/assets/icons/user.svg'
+                    iconAlt='user'
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.INPUT}
+                    control={form.control}
+                    name="email"
+                    label="Email"
+                    placeholder="andres@gmail.com"
+                    iconSrc="/assets/icons/email.svg"
+                    iconAlt="email"
+                />
+
+                <CustomFormField
+                    fieldType={FormFieldType.PHONE_INPUT}
+                    control={form.control}
+                    name="phone"
+                    label="Phone number"
+                    placeholder="(312) 123-4353"
+                />
+
+                <SubmitButton isLoading={isLoading}>Enviar</SubmitButton>
+            </form>
+        </Form>
+    )
 }
 
 export default PatientForm
